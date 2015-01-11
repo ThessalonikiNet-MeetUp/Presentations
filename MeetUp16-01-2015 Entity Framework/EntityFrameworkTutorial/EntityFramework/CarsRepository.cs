b61fc889cbbd6626c450af5a270036232f4c799a
Babis Karypidis <xabikos@hotmail.com>
2015-01-11 14:05:56 +0200

    Create and implement carsreposiotry through entity framework

---
+++ MeetUp16-01-2015 Entity Framework/EntityFrameworkTutorial/EntityFramework/CarsRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace EntityFrameworkTutorial.EntityFramework
+{
+    class CarsRepository : ICarsRepository
+    {
+        public IQueryable<Car> GetAllCars()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.Cars;
+            }
+        }
+
+        public Car GetCarById(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.Cars.Find(id);
+            }
+        }
+
+        public int InsertCar(Car car)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Cars.Add(car);
+                context.SaveChanges();
+                return car.Id;
+            }
+        }
+
+        public void UpdateCar(Car car)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                if (context.Cars.Count(cc => cc.Id == car.Id) > 0)
+                {
+                    (context.Entry(car)).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void DeleteCar(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var carToDelete = context.Cars.Find(id);
+                if (carToDelete != null)
+                {
+                    context.Cars.Remove(carToDelete);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ MeetUp16-01-2015 Entity Framework/EntityFrameworkTutorial/ICarsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTutorial
+{
+    public interface ICarsRepository
+    {
+        IQueryable<Car> GetAllCars();
+
+        Car GetCarById(int id);
+
+        int InsertCar(Car car);
+
+        void UpdateCar(Car car);
+
+        void DeleteCar(int id);
+    }
+}
