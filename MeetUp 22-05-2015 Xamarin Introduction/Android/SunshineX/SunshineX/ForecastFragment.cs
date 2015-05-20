c7b934c97752eec919b44497db44dc6e2541f873
Ziazios Konstantinos <ziazios@gmail.com>
2015-05-20 18:08:22 +0300

    add rest call to whether api

--- MeetUp 22-05-2015 Xamarin Introduction/Android/SunshineX/SunshineX/ForecastFragment.cs
+++ MeetUp 22-05-2015 Xamarin Introduction/Android/SunshineX/SunshineX/ForecastFragment.cs
@@ -10,6 +10,10 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Json;
 
 namespace SunshineX
 {
@@ -44,8 +48,48 @@ namespace SunshineX
             // Get a reference to the ListView, and attach this adapter to it.
             ListView listView = (ListView)rootView.FindViewById<ListView>(Resource.Id.listview_forecast);
             listView.Adapter = forecastAdapter;
+            OnRefresh();
             return rootView;
             
+        }
+
+        public async Task OnRefresh()
+        {
+            // Get the latitude and longitude entered by the user and create a query.
+            string url = "http://api.geonames.org/findNearByWeatherJSON?lat=" +
+                         "47.7" +
+                         "&lng=" +
+                         "22.2" +
+                         "&username=demo";
+
+            // Fetch the weather information asynchronously, 
+            // parse the results, then update the screen:
+            JsonValue json = await FetchWeatherAsync(url);
+            var ans = json.ToString();
+        }
+
+        // Gets weather data from the passed URL.
+        private async Task<JsonValue> FetchWeatherAsync(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            }
         }
     }
 }
